a6506eff052f6400d66071cdeca7e6ece344fb05
Saul <scalleja12345@gmail.com>
2024-09-17 18:33:41 -0400

    made movie app

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+import MovieDisplay from "./MovieDisplay";
+import Form from "./Form";
+
+export default function App() {
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
